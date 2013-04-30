20d4c8cba08da1b40713bb14054eaa48cc878ea6
Grigory Chernyshev <systray@yandex.ru>
2013-04-30 12:26:25 +0400

    internal: test avatar image injection

--- server/resources/js/avatar.js
+++ server/resources/js/avatar.js
@@ -1,1 +1,3 @@
 console.log("avatar javascript resource loaded successfully!");
+
+$j("#beforeUserId").after('<img src="http://www.animated-gifs.eu/avatars-100x100-cartoons-mickey/0024.gif" alt="Goofy" height="18" width="18">');
