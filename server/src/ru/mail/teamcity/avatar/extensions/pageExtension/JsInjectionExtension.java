ac62b2032ac98046bbf4d1219500915b1a49e38e
Grigory Chernyshev <systray@yandex.ru>
2013-05-03 17:20:52 +0400

    internal: add NotNull annotation

--- server/src/ru/mail/teamcity/avatar/extensions/pageExtension/JsInjectionExtension.java
+++ server/src/ru/mail/teamcity/avatar/extensions/pageExtension/JsInjectionExtension.java
@@ -4,6 +4,7 @@ import jetbrains.buildServer.web.openapi.PagePlaces;
 import jetbrains.buildServer.web.openapi.PlaceId;
 import jetbrains.buildServer.web.openapi.PluginDescriptor;
 import jetbrains.buildServer.web.openapi.SimplePageExtension;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * User: Grigory Chernyshev
@@ -11,7 +12,7 @@ import jetbrains.buildServer.web.openapi.SimplePageExtension;
  */
 public class JsInjectionExtension extends SimplePageExtension {
 
-  public JsInjectionExtension(PagePlaces pagePlaces, PluginDescriptor descriptor) {
+  public JsInjectionExtension(@NotNull  PagePlaces pagePlaces, @NotNull PluginDescriptor descriptor) {
     super(pagePlaces, PlaceId.ALL_PAGES_FOOTER, "teamcity-avatar", descriptor.getPluginResourcesPath("extension.jsp"));
     addJsFile(descriptor.getPluginResourcesPath("js/avatar.js"));
     register();
