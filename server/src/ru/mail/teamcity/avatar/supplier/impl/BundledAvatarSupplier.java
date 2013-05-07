ea82d7ca2634f70b2e877c8318d56b9edde49a78
Grigory Chernyshev <systray@yandex.ru>
2013-05-07 23:53:11 +0400

    internal: implement bundled avatar support

--- server/src/ru/mail/teamcity/avatar/supplier/impl/BundledAvatarSupplier.java
+++ server/src/ru/mail/teamcity/avatar/supplier/impl/BundledAvatarSupplier.java
@@ -1,16 +1,18 @@
 package ru.mail.teamcity.avatar.supplier.impl;
 
 import jetbrains.buildServer.serverSide.SBuildServer;
+import jetbrains.buildServer.users.PropertyKey;
 import jetbrains.buildServer.users.SUser;
+import jetbrains.buildServer.users.SimplePropertyKey;
 import jetbrains.buildServer.web.openapi.PluginDescriptor;
+import org.apache.commons.io.FilenameUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import ru.mail.teamcity.avatar.supplier.AbstractAvatarSupplier;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,14 +24,17 @@ public class BundledAvatarSupplier extends AbstractAvatarSupplier {
 
   private final String BUNDLED_AVATARS_PATH;
 
+  private final String PROPERTY_KEY_NAME = "avatar." + this.getClass().getSimpleName();
+  private final PropertyKey PROPERTY_KEY = new SimplePropertyKey(PROPERTY_KEY_NAME);
+
   public BundledAvatarSupplier(SBuildServer server, PluginDescriptor pluginDescriptor) {
     BUNDLED_AVATARS_PATH = server.getServerRootPath() + pluginDescriptor.getPluginResourcesPath() + "image/avatars";
   }
 
   @Nullable
   public String getAvatarUrl(SUser user) {
-
-    return null;
+    //  TODO - remove hardcoded plugin URL. And check how it works in admin area.
+    return "plugins/teamcity-avatar/image/avatars/" + user.getPropertyValue(PROPERTY_KEY);
   }
 
   @NotNull
@@ -46,21 +51,30 @@ public class BundledAvatarSupplier extends AbstractAvatarSupplier {
   public Map<String, Object> getTemplateParams(@NotNull SUser user) {
     HashMap<String, Object> params = new HashMap<String, Object>();
 
-    List<String> bundledAvatars = new ArrayList<String>();
+    Map<String, String> bundledAvatars = new HashMap<String, String>();
     File avatarsDir = new File(BUNDLED_AVATARS_PATH);
 
     File[] files = avatarsDir.listFiles();
     if (null != files) {
+      Arrays.sort(files);
       for (File avatar : files) {
-        bundledAvatars.add(avatar.getName());
+        bundledAvatars.put(
+                avatar.getName(),
+                FilenameUtils.removeExtension(avatar.getName())
+        );
       }
     }
 
     params.put("bundledAvatars", bundledAvatars);
+    params.put("selectedAvatar", user.getPropertyValue(PROPERTY_KEY));
     return params;
   }
 
   public void store(SUser user, Map<String, String[]> params) {
-
+    String[] values = params.get("bundledAvatar");
+    if (null != values) {
+      assert values.length > 0;
+      user.setUserProperty(PROPERTY_KEY, values[0]);
+    }
   }
 }
