a437e3750b50338aa98f3d53706186147abc6a8f
Grigory Chernyshev <systray@yandex.ru>
2013-05-08 12:39:29 +0400

    internal: gravatar supplier implementation

---
+++ server/src/ru/mail/teamcity/avatar/supplier/impl/GravatarAvatarSupplier.java
@@ -0,0 +1,56 @@
+package ru.mail.teamcity.avatar.supplier.impl;
+
+import com.timgroup.jgravatar.Gravatar;
+import com.timgroup.jgravatar.GravatarDefaultImage;
+import com.timgroup.jgravatar.GravatarRating;
+import jetbrains.buildServer.users.SUser;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import ru.mail.teamcity.avatar.supplier.AbstractAvatarSupplier;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * User: Grigory Chernyshev
+ * Date: 08.05.13 11:06
+ */
+public class GravatarAvatarSupplier extends AbstractAvatarSupplier {
+
+  private final Gravatar gravatar;
+
+  public GravatarAvatarSupplier() {
+    gravatar = new Gravatar();
+    gravatar.setRating(GravatarRating.GENERAL_AUDIENCES);
+    gravatar.setDefaultImage(GravatarDefaultImage.IDENTICON);
+  }
+
+  @Nullable
+  public String getAvatarUrl(SUser user) {
+    String mail = user.getEmail();
+    if (null != mail) {
+      return gravatar.getUrl(mail);
+    }
+    return null;
+  }
+
+  @NotNull
+  public String getOptionName() {
+    return "Gravatar";
+  }
+
+  @NotNull
+  public String getTemplate() {
+    return "templates/gravatar.vm";
+  }
+
+  @NotNull
+  public Map<String, Object> getTemplateParams(@NotNull SUser user) {
+    HashMap<String, Object> params = new HashMap<String, Object>();
+    params.put("avatarUrl", getAvatarUrl(user));
+    return params;
+  }
+
+  public void store(SUser user, Map<String, String[]> params) {
+  }
+}
