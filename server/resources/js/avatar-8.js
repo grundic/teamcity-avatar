e9de30b543b12f61095950ce0feaf0b58de27b2d
Grigory Chernyshev <systray@yandex.ru>
2013-05-09 10:50:44 +0400

    internal: move common parts to common

--- server/resources/js/avatar-8.js
+++ server/resources/js/avatar-8.js
@@ -1,3 +1,15 @@
+/*
+ * Add avatar for current user to user panel.
+ */
+Avatar.addAvatarToUserPanel = function () {
+  this._getAvatarUrl(null, function (param_hash) {
+    $j("#sp_span_usernamePopup").before('<img src="' + param_hash['avatarUrl'] + '" height="18" width="18">');
+  });
+};
+
+
 $j(document).ready(function () {
-  console.log("Avatar script for TeamCity 8");
+  Avatar.addAvatarToUserPanel();
+  Avatar.showPopupNearElementHack();
+  Avatar.addAvatarToPendingChangesDivTab();
 });
--- server/resources/js/avatar-common.js
+++ server/resources/js/avatar-common.js
@@ -31,5 +31,71 @@ var Avatar = {
             }
     );
 
+  },
+
+  /*
+   * Add avatar for current user to user panel.
+   */
+  addAvatarToUserPanel: function () {
+    this._getAvatarUrl(null, function (param_hash) {
+      $j("#beforeUserId").after('<img src="' + param_hash['avatarUrl'] + '" height="18" width="18">');
+    });
+  },
+
+  /*
+   * Function for hacking standard showPopupNearElement.
+   * We need to inject custom code in callback function,
+   * to be able to modify div content with user avatar.
+   * That's what this function do.
+   */
+  showPopupNearElementHack: function () {
+
+    // we want to add our code after this line
+    var searchString = "element.attr('data-popup', this._name);";
+    // save current function
+    var currentShowPopupNearElement = BS.Popup.prototype.showPopupNearElement.toString();
+    // replace it, adding call to setting avatar function
+    var hackedShowPopupNearElement = currentShowPopupNearElement.replace(searchString, searchString + '\n' + 'Avatar.addAvatarToPendingChanges();');
+
+    // hack it!
+    eval('BS.Popup.prototype.showPopupNearElement = ' + hackedShowPopupNearElement);
+  },
+
+  /*
+   * This function is injected in callback of
+   * popup window with pending changes.
+   * Get username, check for avatar and add it.
+   */
+  addAvatarToPendingChanges: function () {
+    var $this = this;
+    $j('#groupedChanges>div>div>span').each(function () {
+      var username = this.innerHTML;
+      var element_id = "avatar-pending-" + $this._hashCode(username);
+
+      if ($j("#" + element_id).length == 0) {
+        $this._getAvatarUrl(username, function (param_hash) {
+          $j(param_hash['this']).before('<img id="' + element_id + '" src="' + param_hash['avatarUrl'] + '" height="18" width="18">');
+        }, {"this": this});
+      }
+    });
+  },
+
+  addAvatarToPendingChangesDivTab: function () {
+    var $this = this;
+
+    $j($j('#changesTable tbody td.userName span').each(function () {
+      var $span = this;
+      var _onmouseover = $j(this).attr('onmouseover');
+      var match = Avatar.USERNAME_REGEX.exec(_onmouseover);
+      if (match) {
+        var username = match[1];
+        $this._getAvatarUrl(username, function (param_hash) {
+          $j($span).before('<img src="' + param_hash['avatarUrl'] + '" height="18" width="18">');
+        });
+      } else {
+        console.log("[ERROR] Failed to match username: " + _onmouseover);
+      }
+    })
+    );
   }
 };
