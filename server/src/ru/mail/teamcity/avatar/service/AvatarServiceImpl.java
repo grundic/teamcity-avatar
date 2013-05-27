c44c97d733ad34e28630c35e7550242069d5d960
Grigory Chernyshev <systray@yandex.ru>
2013-05-28 00:33:36 +0400

    internal: replace own cache with Guava's

--- server/src/ru/mail/teamcity/avatar/service/AvatarServiceImpl.java
+++ server/src/ru/mail/teamcity/avatar/service/AvatarServiceImpl.java
@@ -1,21 +1,8 @@
-/*
- * Copyright 2000-2013 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package ru.mail.teamcity.avatar.service;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.intellij.openapi.diagnostic.Logger;
 import jetbrains.buildServer.users.PropertyKey;
 import jetbrains.buildServer.users.SUser;
@@ -25,6 +12,7 @@ import org.jetbrains.annotations.Nullable;
 import ru.mail.teamcity.avatar.supplier.AvatarSupplier;
 
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 /**
  * User: Grigory Chernyshev
@@ -35,12 +23,25 @@ public class AvatarServiceImpl implements AvatarService {
   private final static Logger LOG = Logger.getInstance(AvatarServiceImpl.class.getName());
 
   private final Map<String, AvatarSupplier> suppliers;
+  private final LoadingCache<SUser, String> cache;
 
   private final String PROPERTY_KEY_NAME = "avatar.selected.supplier.type";
   private final PropertyKey PROPERTY_KEY = new SimplePropertyKey(PROPERTY_KEY_NAME);
 
-  public AvatarServiceImpl(Map<String, AvatarSupplier> suppliers) {
+  public AvatarServiceImpl(@NotNull Map<String, AvatarSupplier> suppliers, @NotNull CacheBuilder cacheBuilder) {
     this.suppliers = suppliers;
+
+    //noinspection unchecked
+    this.cache = cacheBuilder.build(
+            new CacheLoader<SUser, String>() {
+              public String load(SUser user) {
+                AvatarSupplier avatarSupplier = getAvatarSupplier(user);
+                if (null == avatarSupplier) {
+                  return "";
+                }
+                return avatarSupplier.getAvatarUrl(user);
+              }
+            });
   }
 
   @Nullable
@@ -66,20 +67,16 @@ public class AvatarServiceImpl implements AvatarService {
   public void store(@NotNull SUser user, @NotNull AvatarSupplier avatarSupplier, @NotNull Map<String, String[]> params) {
     user.setUserProperty(PROPERTY_KEY, avatarSupplier.getBeanName());
     avatarSupplier.store(user, params);
-    AvatarCache.setCache(user.getUsername(), avatarSupplier.getAvatarUrl(user));
+    cache.put(user, avatarSupplier.getAvatarUrl(user));
   }
 
   @NotNull
   public String getAvatarUrl(@NotNull SUser user) {
-    String avatarUrl = AvatarCache.getCache(user.getUsername());
-    if (null == avatarUrl) {
-      AvatarSupplier avatarSupplier = getAvatarSupplier(user);
-      if (null == avatarSupplier) {
-        return "";
-      }
-      avatarUrl = avatarSupplier.getAvatarUrl(user);
-      AvatarCache.setCache(user.getUsername(), avatarUrl);
+    try {
+      return cache.get(user);
+    } catch (ExecutionException e) {
+      LOG.error(e.getCause());
+      return "";
     }
-    return avatarUrl;
   }
 }
